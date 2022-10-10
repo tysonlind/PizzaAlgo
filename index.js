91b3feeea7dead2c3cadace1966f2c75c9110cfe
tyson lind <tysonfloydlind@gmail.com>
2022-10-10 14:19:27 -0600

    fixed a comment typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // 2. Decide which type of pizza to order.
 
 //3. these are some bug fix changes added through the bugFix branch
-aha! theres that pesky bug!
+//aha! theres that pesky bug!
 function bugKill (bug, flySwatter){
     console.log(bug + " " + "has been killed using a" + " " + flySwatter);
 }
