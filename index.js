48bfb87aae8fc7b96f09e544bd2736b4e880a67b
tyson lind <tysonfloydlind@gmail.com>
2022-10-10 14:18:56 -0600

    added a bug killing function

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // 1. Decide which business to order from
 // 2. Decide which type of pizza to order.
+
+//3. these are some bug fix changes added through the bugFix branch
+aha! theres that pesky bug!
+function bugKill (bug, flySwatter){
+    console.log(bug + " " + "has been killed using a" + " " + flySwatter);
+}
+bugKill();
